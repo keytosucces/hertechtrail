93436a9535219b088e0fb8a77cafbf1166c72507
key muha <91820369+keytosucces@users.noreply.github.com>
2022-11-23 19:56:41 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,55 @@
+const embellish = document.getElementById("img1");
+
+embellish.addEventListener("click", (event) => {
+    event.target.style.background = "darkgreen";
+    event.target.style.color = "white";
+});
+
+const embellish2 = document.getElementById("img2");
+
+embellish2.addEventListener("click", (event) => {
+    event.target.style.background = "yellow";
+    event.target.style.color = "lightblue";
+});
+
+const embellish3 = document.getElementById("img3");
+
+embellish3.addEventListener("click", (event) => {
+    event.target.style.background = "darkred";
+    event.target.style.color = "azure";
+});
+
+const embellish4 = document.getElementById("img4");
+
+embellish4.addEventListener("click", (event) => {
+    event.target.style.background = "yellow";
+    event.target.style.color = "red";
+});
+
+const embellish5 = document.getElementById("img5");
+
+embellish5.addEventListener("click", (event) => {
+    event.target.style.background = "blue";
+    event.target.style.color = "burlywood";
+});
+
+const embellish6 = document.getElementById("img6");
+
+embellish6.addEventListener("click", (event) => {
+    event.target.style.background = "beige";
+    event.target.style.color = "green";
+});
+
+const embellish7 = document.getElementById("img7");
+
+embellish7.addEventListener("click", (event) => {
+    event.target.style.background = "lightblue";
+    event.target.style.color = "orange";
+});
+
+const embellish8 = document.getElementById("img8");
+
+embellish8.addEventListener("click", (event) => {
+    event.target.style.background = "rgb(74, 35, 35)";
+    event.target.style.color = "pink";
+});
